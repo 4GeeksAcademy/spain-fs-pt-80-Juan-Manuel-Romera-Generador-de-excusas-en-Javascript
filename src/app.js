7877f5e30f245443cfa83dfac7fec751cc8c57d0
Tradeobrih <romera.valladolid@gmail.com>
2024-10-09 11:37:17 +0000

    tercer commit

--- src/app.js
+++ src/app.js
@@ -8,17 +8,26 @@ import "./assets/img/4geeks.ico";
 const excuse = document.querySelector("#excuse");
 const button = document.querySelector("#generate-btn");
 
-const updateExcuse = () => { excuse.innerHTML = generateExcuse(); };
-window.onload = () => { updateExcuse(); };
+const updateExcuse = () => {
+  excuse.innerHTML = generateExcuse();
+};
+window.onload = () => {
+  updateExcuse();
+};
 button.addEventListener("click", updateExcuse);
 
 const phrases = [
-    ["The dog", "My grandma", "The mailman", "My bird"],
-    ["ate", "peed", "crushed", "broke"],
-    ["my homework", "my phone", "the car"],
-    ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"]
+  ["The dog", "My grandma", "The mailman", "My bird"],
+  ["ate", "peed", "crushed", "broke"],
+  ["my homework", "my phone", "the car"],
+  [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ]
 ];
 
 const getValue = arr => arr[Math.floor(Math.random() * arr.length)];
 const generateExcuse = () => phrases.map(getValue).join(" ");
-
